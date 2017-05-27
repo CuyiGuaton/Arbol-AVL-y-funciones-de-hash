5be6f1f84aa3e7fba5a494d78d912428d3638bd4
Sergio Salinas <sergio.salinas@usach.cl>
2017-05-27 02:18:20 -0400

    terminada todos las funciones de la lista

--- linkedlist-test.c
+++ linkedlist-test.c
@@ -2,16 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAXchar 30
+
 typedef struct node{
-    char data[30];
+    char data[MAXchar];
     //int freq;
     struct node* next;
 } node;
 
 typedef void (*callback)(node* data);
 
+void buscarMayorFreq(node* head, int *mayorFreq, char mayor[MAXchar] ); //busca la palabra que más se repite en la lista
+node* create(char data[MAXchar], node* next); //crea un nodo nuevo
+node* prepend(node* head,char data[MAXchar]); // insterta un elemento al inicio de la lista
+void mostrarfreq(node* head); // muestra las palabras y sus frecuencias en la lista
+void mostrar(node* head); // muestra todos los elementos de la lista
+node* search(node* head,char data[MAXchar]); // busca un elemento en la lista
+int contar(node *head); //cuenta los elementos de la lista
+void dispose(node *head); // borra la lista
+
 //crea un nodo
-node* create(char data[30], node* next){
+node* create(char data[MAXchar], node* next){
     node* new_node = (node*)malloc(sizeof(node));
     if(new_node == NULL){
         printf("Error creating a new node.\n");
@@ -24,7 +35,7 @@ node* create(char data[30], node* next){
 }
 
 // agrega un nodo al inicio de la lista
-node* prepend(node* head,char data[30]){
+node* prepend(node* head,char data[MAXchar]){
     node* new_node = create(data,head);
     head = new_node;
     return head;
@@ -33,7 +44,7 @@ node* prepend(node* head,char data[30]){
 //muestra los elementos en la lista y su frecuencia en estos
 void mostrarfreq(node* head){
     node* cursor = head;
-    char aux[30];
+    char aux[MAXchar];
     int freq=1;
     if(cursor != NULL){ // obtiene el primer elemento de la lsita y lo guarda en un auxiliar
       strcpy(aux, cursor->data);
@@ -54,27 +65,63 @@ void mostrarfreq(node* head){
 }
 
 //muestra los elementos de la lista dinamica
-void mostrar(node* head)
-{
+void mostrar(node* head){
     node* cursor = head;
-    while(cursor != NULL)
-    {
+    while(cursor != NULL){
         printf("%s ", cursor->data);
         cursor = cursor->next;
     }
 }
 
+//muestra los elementos de la lista dinamica
+void buscarMayorFreq(node* head, int *mayorFreq, char mayor[MAXchar]){
+  node* cursor = head;
+  char aux[MAXchar];
+  int freq=1;
+  if(cursor != NULL){ // obtiene el primer elemento de la lsita y lo guarda en un auxiliar
+    strcpy(aux, cursor->data);
+    cursor = cursor->next;
+  }
+  while(cursor != NULL){ //recorre la lista
+      if(strcmp(cursor->data, aux) == 0) //si el elemento siguiente es igual que el anterior aumenta la frecuencia
+        freq++;
+      else{ //si el elemento siguiente es distinto entonces muestra el elemento y la frecuencia
+        if (freq > (*mayorFreq)){
+            (*mayorFreq) = freq;
+            strcpy(mayor, cursor->data);
+        }
+        strcpy(aux, cursor->data);
+        freq=1;
+      }
+  cursor = cursor->next;
+  }
+  if (freq > (*mayorFreq)){
+      (*mayorFreq) = freq;
+      strcpy(mayor, aux);
+  }
+
+}
+
 //busca un elemento en el nodo
-node* search(node* head,char data[30])
-{
+node* search(node* head,char data[MAXchar]){
     node *cursor = head;
-    while(cursor!=NULL)
-    {
+    while(cursor!=NULL){
         if(strcmp(  cursor->data, data) ==0)
             return cursor;
         cursor = cursor->next;
     }
     return NULL;
+}
+
+// cuenta los elementos de la lista
+int contar(node *head){
+    node *cursor = head;
+    int c = 0;
+    while(cursor != NULL){
+        c++;
+        cursor = cursor->next;
+    }
+    return c;
 }
 
 //borra la listga
@@ -95,12 +142,22 @@ int main(){
     node* head = NULL;
     node* tmp = NULL;
 
-    head = prepend(head, "pico");
-    head = prepend(head, "pico");
-    head = prepend(head, "tula");
+    head = prepend(head, "hola");
+    head = prepend(head, "hola");
+    head = prepend(head, "chao");
+    printf("Mostrando nodos:\n" );
     mostrar(head);
-    printf("\n" );
+    printf("\nMostrar nodos repetidos\n" );
     mostrarfreq(head);
+    printf("\nMostrar el nodo que más se repite\n" );
+    char mayor[MAXchar];
+    int mayorFreq=1;
+    buscarMayorFreq(head, &mayorFreq, mayor); //se envia la frecuencia mayor y el string por refencia para así obtener dos valores de la función
+    printf("%s - %i\n", mayor,mayorFreq);
+    int count= contar(head);
+    printf("La cantidad de elementos de la lista es: %i", count);
+    if( search(head,"hola") != NULL)
+      printf("\nLa palabra %s se encuentra en la lista\n", "hola");
     dispose(head);
    return 0;
 }
